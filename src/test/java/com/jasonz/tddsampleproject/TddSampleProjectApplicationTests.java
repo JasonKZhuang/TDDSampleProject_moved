83d962ddd8bc0b40adc3a45bc2bc6e57d7c62c05
Jason Zhuang <zhuangkaizhi@gmail.com>
2022-06-02 00:05:10 +1000

    update

---
+++ src/test/java/com/jasonz/tddsampleproject/TddSampleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jasonz.tddsampleproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddSampleProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
