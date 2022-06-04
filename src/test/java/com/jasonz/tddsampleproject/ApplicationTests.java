e30ceaa5bd17bb83e68464e42bdd2c075dbc1e3f
Jason Zhuang <zhuangkaizhi@gmail.com>
2022-06-05 08:02:39 +1000

    Update ApplicationTests.java

---
+++ src/test/java/com/jasonz/tddsampleproject/ApplicationTests.java
@@ -0,0 +1,21 @@
+package com.jasonz.tddsampleproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * reference:
+ * https://www.youtube.com/watch?v=s9vt6UJiHg4
+ * https://www.youtube.com/watch?v=z6gOPonp2t0
+ * https://www.youtube.com/watch?v=Geq60OVyBPg
+ * https://www.youtube.com/watch?v=MRG6rm54lhA
+ * https://www.youtube.com/watch?v=Hh17JDpsKqc
+ */
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
